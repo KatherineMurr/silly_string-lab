181d3596bf38f5d6f2f25b1ad61c7d8aa38d8d31
Katherine Murr <katherinemurr@yahoo.com>
2017-06-06 15:24:40 +0000

    finished the silly string lab from kode with klossy 2017

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,13 @@
-#code your solutions here
-
-
-
+my_word = "coding"
+puts my_word.upcase
+shouting = "IM NOT SHOUTING"
+puts shouting.downcase
+karlie = "Karlie Kloss"
+puts karlie.length
+puts "wow" + " mom"
+programming = "i really like programming"
+puts programming.capitalize
+long = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+puts long.gsub("A", "0")
+puts long.gsub("a", "o")
+puts 3
